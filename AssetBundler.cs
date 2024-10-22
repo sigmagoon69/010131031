a458b6eb92c4230c859827359477af5bb4557f3b
Myssal <143514975+myssal@users.noreply.github.com>
2024-10-22 23:07:55 +0700

    feat: add shortcut and build directiory

--- AssetBundler.cs
+++ AssetBundler.cs
@@ -5,17 +5,22 @@ using System.IO;
 using UnityEngine;
 using UnityEditor;
 
-
 public class AssetBundler : MonoBehaviour
 {
-    
-    public static string outputPath = "Assets/";
-
-    [MenuItem("Assets/Build Asset Bundles")]
+    [MenuItem("Assets/Build Asset Bundles #b")]
     static void BuildAssetBundle()
-    {        
+    {   
+        // create output path
+        string outputPath = "Assets/Bundles";
+        Directory.CreateDirectory(outputPath);
         BuildPipeline.BuildAssetBundles(outputPath, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
-        Debug.Log("SUCCESS, CHECK OUTPUT PATH");
+        Debug.Log($"Build successfully, check {outputPath}");
+
+        // log asset bundles list
+        foreach (string bundle in AssetDatabase.GetAllAssetBundleNames())
+        {
+            Debug.Log($"Asset bundle: {bundle}");
+        }
     }
 }
 #endif
